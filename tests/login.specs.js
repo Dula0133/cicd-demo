3cf6dbfeaab05f708e4a0c15ed7bc9d56c91da7a
qachapterdemo <info.randikabls@gmail.com>
2026-02-22 05:24:45 +0530

    Create login.specs.js

---
+++ tests/login.specs.js
@@ -0,0 +1,14 @@
+const { test, expect } = require('@playwright/test');
+const path = require('path');
+
+test('Valid login should succeed', async ({ page }) => {
+
+  const filePath = 'file://' + path.resolve(__dirname, '../index.html');
+  await page.goto(filePath);
+
+  await page.fill('#username', 'qa');
+  await page.fill('#password', '1234');
+  await page.click('#loginBtn');
+
+  await expect(page.locator('#message')).toHaveText('Login Successful');
+});
